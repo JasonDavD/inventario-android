4b7627d31c46460f97519427d1e332fcfff78b05
JasonDavD <jason.dav.del@gmail.com>
2026-02-19 18:51:56 -0500

    feat: agregar campo idApi al modelo Producto con @SerializedName para mapeo con API

--- app/src/main/java/pe/com/cibertec/inventarioferreteriazamora/modelos/Producto.kt
+++ app/src/main/java/pe/com/cibertec/inventarioferreteriazamora/modelos/Producto.kt
@@ -1,7 +1,11 @@
 package pe.com.cibertec.inventarioferreteriazamora.modelos
 
+import com.google.gson.annotations.SerializedName
+
 data class Producto(
     var cod: Int = 0,
+    @SerializedName("id")
+    var idApi: Int = 0,
     var nombre: String = "",
     var categoria: String = "",
     var precio: Double = 0.0,
